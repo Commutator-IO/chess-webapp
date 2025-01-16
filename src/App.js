e1e7193b97baff081de3e334bf04ab0a2462ea31
Michel Hua <mycaule@users.noreply.github.com>
2025-01-16 16:53:31 +0100

    day 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chessboard } from 'react-chessboard';
 import { Chess } from 'chess.js';
 
 const App = () => {
   const [game, setGame] = useState(new Chess());
+  const [stockfish, setStockfish] = useState(null);
+  const [bestMove, setBestMove] = useState("");
+
+  useEffect(() => {
+    const stockfishWorker = new Worker("/js/stockfish-16.1-lite-single.js");
+    setStockfish(stockfishWorker);
+
+    stockfishWorker.onmessage = (event) => {
+      const message = event.data;
+      if (message.startsWith("bestmove")) {
+        const move = message.split(" ")[1];
+        setBestMove(move); // best move to display
+      }
+    };
+
+    return () => {
+      stockfishWorker.terminate();
+    };
+  }, []);
 
   // Handling piece movements on the chessboard
   const onDrop = (sourceSquare, targetSquare) => {
@@ -23,6 +42,12 @@ const App = () => {
 
       // when valid move, update the game state with the new position
       setGame(gameCopy);
+
+      if (stockfish) {
+        stockfish.postMessage(`position fen ${gameCopy.fen()}`); // Send the board position in FEN format
+        stockfish.postMessage("go depth 15"); // Instruct Stockfish to analyze the position up to a depth of 15 moves
+      }
+
       return true; // valid move
     } catch (error) {
       console.error(error.message);
@@ -32,12 +57,17 @@ const App = () => {
 
   return (
     <div>
-      <h1>Chess Game</h1>
-      <Chessboard
-        position={game.fen()}      // Set the chessboard position to the current game state
-        onPieceDrop={onDrop}       // Trigger the onDrop function when a piece is moved
-        boardWidth={500}           // Set the width of the chessboard to 500px
-      />
+      <div>
+        <h1>Chess Game</h1>
+        <Chessboard
+          position={game.fen()}      // Set the chessboard position to the current game state
+          onPieceDrop={onDrop}       // Trigger the onDrop function when a piece is moved
+          boardWidth={500}           // Set the width of the chessboard to 500px
+        />
+      </div>
+      <div>
+        <h3>Best Move: {bestMove || "Calculating..."}</h3>
+      </div>
     </div>
   );
 };
